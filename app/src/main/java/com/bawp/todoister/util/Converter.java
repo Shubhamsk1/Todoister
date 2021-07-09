b73fde314effbd3e350b50639169d6f52058bd22
Shubhamsk1 <shubahmsk1@gmail.com>
2021-07-09 11:26:00 +0530

    Completed

---
+++ app/src/main/java/com/bawp/todoister/util/Converter.java
@@ -0,0 +1,34 @@
+package com.bawp.todoister.util;
+
+import java.util.Date;
+
+import androidx.room.TypeConverter;
+
+import com.bawp.todoister.model.Priority;
+
+
+
+public class Converter {
+
+    @TypeConverter
+    public static Date fromTimestamp(Long value) {
+        return value == null ? null : new Date(value);
+    }
+
+    @TypeConverter
+    public static Long dateToTimestamp(Date date) {
+        return date == null ? null : date.getTime();
+    }
+
+    @TypeConverter
+    public static String fromPriority(Priority priority) {
+        return priority == null ? null : priority.name();
+    }
+
+    @TypeConverter
+    public static Priority toPriority(String priority) {
+        return priority == null ? null : Priority.valueOf(priority);
+    }
+}
+
+
